803e9fca30515746b57b311a165c85a99b890efc
cheuyin <yinstanleycheung@gmail.com>
2026-05-17 10:05:51 -0700

    Connect to Gemini API and send test request

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from ai-agent-python!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("API key is invalid.")
+
+    client = genai.Client(api_key=api_key)
+
+    res = client.models.generate_content(
+        model="gemini-3.1-flash-lite",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+
+    if res.usage_metadata is None:
+        raise RuntimeError("API request failed: invalid response.")
+
+    print("Prompt tokens:", res.usage_metadata.prompt_token_count)
+    print("Response tokens: ", res.usage_metadata.candidates_token_count)
+
+    print(res.text)
 
 
 if __name__ == "__main__":
