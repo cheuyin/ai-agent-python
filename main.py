435f62d35dc2d60c15277de49f184cdad29af108
cheuyin <yinstanleycheung@gmail.com>
2026-05-20 13:06:30 -0700

    refactor: move function declarations to new call_function.py file

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import argparse
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from call_function import available_functions
 
 
 def main():
@@ -41,11 +38,6 @@ def main():
 
     messages = [types.Content(
         role="user", parts=[types.Part(text=args.user_prompt)])]
-
-    available_functions = types.Tool(
-        function_declarations=[
-            schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file],
-    )
 
     res = client.models.generate_content(
         model="gemini-3.1-flash-lite",
